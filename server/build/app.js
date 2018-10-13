398d11a1ba34199a24e74d8078c7459ccdc158d6
kunal-mandalia <kunal.v.mandalia@gmail.com>
2018-10-13 19:15:59 +0100

    fix: logger import

--- server/build/app.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function sqaure(n) {
-    return n * n;
-}
-exports.sqaure = sqaure;
--- server/src/app.ts
+++ server/src/app.ts
@@ -1,3 +1,13 @@
-export function sqaure(n: number): number {
-  return n * n;
-}
+import * as express from 'express';
+import { logger } from './logger/index';
+
+const PORT = process.env.PORT || 8080;
+const app = express();
+
+app.get('/status', (req, res) => {
+  return res.json({ status: 200 });
+})
+
+app.listen(PORT, () => {
+  logger.info(`homeless-household-server running on port ${PORT}`);
+})
