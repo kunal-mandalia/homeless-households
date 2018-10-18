f6f9291f07bd65db1aeed0aed86542f6b4c9e133
Kunal Mandalia <kunal.v.mandalia@gmail.com>
2018-10-18 20:57:44 +0100

    fix: cors

--- web-client/src/App.tsx
+++ web-client/src/App.tsx
@@ -4,6 +4,37 @@ import './App.css';
 import logo from './logo.svg';
 
 class App extends React.Component {
+
+  public componentDidMount() {
+    this.getInitialData();
+  }
+
+  public async getInitialData() {
+
+    const request = {
+      query: 'query { getHomelessHouseholds(input:{limit:10, offset:0}) { id }}',
+      variables: null,
+    }
+
+    await fetch(
+      'http://localhost:8080/graphql',
+      {
+        body: JSON.stringify(request),
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        method: 'post',
+      })
+      .then((response) => {
+        // tslint:disable-next-line:no-console
+        console.log(">>> response", response)
+        const data = response.json();
+        // tslint:disable-next-line:no-console
+        console.log("response data", data);
+      });
+  }
+
   public render() {
     return (
       <div className="App">
