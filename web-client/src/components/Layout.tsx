181d2b29d598ec2d78466efa03613950ae36eccf
kunal-mandalia <kunal.v.mandalia@gmail.com>
2018-10-27 14:35:13 +0100

    refactor: homelesshouseholds container - component split

--- web-client/src/components/Layout.tsx
+++ web-client/src/components/Layout.tsx
@@ -1,7 +1,11 @@
 import * as React from 'react';
 import styled from 'styled-components';
 import { COLORS } from '../constants';
-import { Filters } from './Filters';
+
+interface IProps {
+  sidebar: JSX.Element,
+  tiles: JSX.Element[],
+}
 
 const LayoutWrapper = styled.div`
   display: flex;
@@ -10,13 +14,13 @@ const LayoutWrapper = styled.div`
   min-height: 100vh;
 `
 
-const Sidebar = styled.div`
+const SidebarWrapper = styled.div`
   background-color: ${COLORS.MAROON};
   flex-basis: 385px;
   flex-grow: 0;
 `
-  
-const Tiles = styled.div`
+
+const TilesWrapper = styled.div`
   display: flex;
   flex: 1;
   flex-grow: 8;
@@ -26,44 +30,19 @@ const Tiles = styled.div`
 
 const Tile = styled.div`
   flex: 1;
+  flex-direction: column;
   flex-basis: 300px;
   border: solid 4px ${COLORS.MAROON};
-  height: 50%;
   min-height: 200px;
   background-color: ${COLORS.MAROON_LIGHT};
 `
 
-interface ILayoutProps {
-  count: number,
-  data: IHomelessHouseholds[],
-  filters: IFilters,
-  filteredData: IHomelessHouseholds[],
-  total: number,
-  handleFilterChange(filter: string, value: any): void,
-}
 
-const tiles = Array(5).fill(0).map((_, id) => id);
-
-export const Layout: React.SFC<ILayoutProps> = ({
-  count,
-  data,
-  filteredData,
-  filters,
-  handleFilterChange,
-  total,
-}) => (
-  <LayoutWrapper>
-    <Tiles>
-      {tiles.map(t => <Tile key={t} />)}
-    </Tiles>
-    <Sidebar>
-      <Filters
-        count={count}
-        data={data}
-        filters={filters}
-        onFilterChange={handleFilterChange}
-        total={total}
-      />
-    </Sidebar>
+export const Layout = ({ sidebar, tiles } : IProps) => {
+  return <LayoutWrapper>
+    <SidebarWrapper>{sidebar}</SidebarWrapper>
+    <TilesWrapper>
+      {tiles.map((tile, id) => <Tile key={id} data-count={tiles.length}>{tile}</Tile>)}
+    </TilesWrapper>
   </LayoutWrapper>
-)
+}
--- web-client/src/components/LoadingIndicator.tsx
+++ web-client/src/components/LoadingIndicator.tsx
@@ -1,11 +1,12 @@
 import { Icon, Spin } from 'antd';
 import * as React from 'react';
 import styled from 'styled-components';
-
+import { COLORS } from '../constants';
 
 const antIcon = <Icon type="loading" style={{ fontSize: '5em' }} spin={true} />;
 
 const SpinWrapper = styled.div`
+  background-color: ${COLORS.MAROON};
   display: flex;
   width: 100vw;
   height: 100vh;
