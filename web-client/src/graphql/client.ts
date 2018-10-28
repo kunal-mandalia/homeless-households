04bda350946da4201d86b92504645a4c5bcf54a7
kunal-mandalia <kunal.v.mandalia@gmail.com>
2018-10-28 19:27:20 +0000

    feat: use apollo-link-state

--- web-client/src/graphql/client.ts
+++ web-client/src/graphql/client.ts
@@ -1,8 +1,23 @@
-import ApolloClient from 'apollo-boost';
-import { RUNTIME_VARIABLES } from '../config/runtime'
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { ApolloClient } from 'apollo-client';
+import { ApolloLink } from 'apollo-link';
+import { HttpLink } from 'apollo-link-http';
+import { withClientState } from 'apollo-link-state';
+import { RUNTIME_VARIABLES } from '../config/runtime';
+import { defaults, resolvers } from './resolvers';
+import { typeDefs } from './typedefs';
+
+const cache = new InMemoryCache();
+
+const stateLink = withClientState({ resolvers, defaults, cache, typeDefs });
+
+const httpLink = new HttpLink({
+  uri: RUNTIME_VARIABLES.API_HOST,
+});
 
 const client = new ApolloClient({
-  uri: RUNTIME_VARIABLES.API_HOST
+  cache,
+  link: ApolloLink.from([stateLink, httpLink])
 });
 
 export default client;
