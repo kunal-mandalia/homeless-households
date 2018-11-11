22bcb692db71c7e108be9cb638cc8c857e1709da
kunal-mandalia <kunal.v.mandalia@gmail.com>
2018-11-11 22:40:24 +0000

    feat: filter homelesshouseholds and update local state

---
+++ web-client/src/graphql/__fixtures__/filters.ts
@@ -0,0 +1,9 @@
+export const filters = {
+  __typename: "Filters",
+  ageRange: [-Infinity, +Infinity],
+  decision: null,
+  ethnicity: null,
+  nationality: null,
+  need: null,
+  reason: null,
+}
